e13f2c3af9a669462cd28096c3c8b2d91f61e897
Srishti Singh <115684689+Srishti2301@users.noreply.github.com>
2024-01-14 20:20:24 +0530

    Add files via upload

---
+++ model/data.js
@@ -0,0 +1,14 @@
+const songs = [
+    {
+        title: 'Kesariya',
+        artist:'Arijit Singh',
+        image: require("../assets/kesariya.png"),
+        id:'1',
+    },
+    {
+        title:'Lag Jaa Gale',
+        artist:'Lata Mangeshkar',
+        image: require("../assets/lag-jaagale.png"),
+        id: '2',
+    }
+]
